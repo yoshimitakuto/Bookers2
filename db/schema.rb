128e05851be82bdd9a98b7a6ee31e695e3a3cfcc
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-08 08:22:35 +0000

    データベース上に必要なテーブルを作成完了

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2023_04_08_065440) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
