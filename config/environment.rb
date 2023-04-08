b2ce8a8f0020b66390fc860a94c1a2c47ba30d60
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-08 10:20:29 +0000

    Bootstrapを使うために必要な記述を追加する完了

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+const webpack = require('webpack')
+environment.plugin.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
