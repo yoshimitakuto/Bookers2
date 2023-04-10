2041dae529fab2bc1efc41fa7a6607d88245a3e3
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-04-10 09:05:09 +0000

    4/10一旦保存

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,8 +16,15 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     book = Book.find(params[:id])
     # @book.user_id = current_user.id
